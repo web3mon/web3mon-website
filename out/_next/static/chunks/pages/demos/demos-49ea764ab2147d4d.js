f131300a32285f1a6855913ec3a08e7c7fe5833e
web3mon <hello@web3mon.io>
2022-07-15 10:53:03 +0900

    out folder modification

--- out/_next/static/chunks/pages/demos/demos-49ea764ab2147d4d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[736],{508:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/demos/demos",function(){return u(1768)}])}},function(n){n.O(0,[231,768,774,888,179],(function(){return _=508,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
